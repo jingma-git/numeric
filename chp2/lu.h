8115730e81915fa1307aabfda8ebc8a5618d1400
jingma-git <11921154@zju.edu.cn>
2021-07-13 20:55:00 +0800

    partial pivoting: PA = LU

--- chp2/lu.h
+++ chp2/lu.h
@@ -33,3 +33,105 @@ void lu(const Eigen::MatrixXd &A, Eigen::MatrixXd &L, Eigen::MatrixXd &U)
         }
     }
 }
+
+// chp2.4 Paritial Pivot LU, select the largest pivot from the rest rows
+// PA = LU
+class PartialPivotLU
+{
+
+public:
+    PartialPivotLU(const Eigen::MatrixXd &mat) : A(mat) {}
+    const Eigen::MatrixXd &matrixL() const { return L; }
+    const Eigen::MatrixXd &matrixU() const { return U; }
+    const Eigen::MatrixXd &matrixP() const { return P; }
+
+    void compute() // factorize
+    {
+        using namespace Eigen;
+        int n = A.rows();
+        assert(A.rows() == A.cols() && "Input matrix A must be square!");
+        MatrixXd a = A;
+        P = MatrixXd::Identity(n, n);
+
+        std::cout << "a\n"
+                  << a << std::endl;
+
+        for (int j = 0; j < n - 1; ++j)
+        {
+            for (int i = j + 1; i < n; ++i)
+            {
+                // select the largest element as pivot from col_j
+                double pivot = a(j, j);
+                int pi = j;
+                for (int k = j + 1; k < n; ++k)
+                {
+                    if (a(k, j) > pivot)
+                    {
+                        pivot = a(k, j);
+                        pi = k;
+                    }
+                }
+                if (pi != j)
+                {
+                    P.row(j).swap(P.row(pi));
+                    a.row(j).swap(a.row(pi));
+                }
+
+                // eliminate pivot by subtract
+                double mult = a(i, j) / a(j, j);
+                a(i, j) = mult;
+                for (int k = j + 1; k < n; ++k)
+                {
+                    a(i, k) = a(i, k) - mult * a(j, k);
+                }
+            }
+        }
+
+        std::cout << "after a\n"
+                  << a << std::endl;
+        L = MatrixXd::Identity(n, n);
+        L.triangularView<StrictlyLower>() = a.triangularView<StrictlyLower>();
+        std::cout << "after L\n"
+                  << L << std::endl;
+        U = a.triangularView<Upper>();
+        std::cout << "after U\n"
+                  << U << std::endl;
+        std::cout << "after P\n"
+                  << P << std::endl;
+    }
+
+    Eigen::VectorXd solve(const Eigen::VectorXd &rhs)
+    {
+        int n = A.rows();
+        // L*c = P*rhs
+        VectorXd b = P * rhs;
+        for (int i = 0; i < n; ++i)
+        {
+            for (int j = 0; j < i; ++j)
+            {
+                b(i) -= L(i, j) * b(j);
+            }
+            b(i) = b(i) / L(i, i);
+        }
+        std::cout << "c\n"
+                  << b.transpose() << std::endl;
+
+        for (int i = n - 1; i >= 0; --i)
+        {
+            for (int j = n - 1; j > i; --j)
+            {
+                b(i) -= U(i, j) * b(j);
+            }
+            b(i) = b(i) / U(i, i);
+        }
+        std::cout << "x\n"
+                  << b.transpose() << std::endl;
+        return b;
+    }
+
+private:
+    Eigen::MatrixXd A;
+    Eigen::MatrixXd L;
+    Eigen::MatrixXd U;
+    Eigen::MatrixXd P;
+};
--- chp2/main.cpp
+++ chp2/main.cpp
@@ -33,6 +33,17 @@ void test_data2(MatrixXd &A, VectorXd &b)
     b << 2, 4, 2;
 }
 
+// eg.2.17
+void test_data3(MatrixXd &A, VectorXd &b)
+{
+    A.resize(3, 3);
+    A << 2, 1, 5,
+        4, 4, -4,
+        1, 3, 1;
+    b.resize(3, 1);
+    b << 5, 0, 6;
+}
+
 int main()
 {
     MatrixXd A;
@@ -42,20 +53,27 @@ int main()
     // naive_gaussian(A, b, x);
     // cout << "sol=" << x.transpose() << endl;
 
-    test_data2(A, b);
-    // Eigen::PartialPivLU<MatrixXd> LU(A);
+    // test_data2(A, b);
+    test_data3(A, b);
+    // Eigen::PartialPivLU<MatrixXd> LU(A); // chp2.4
     // MatrixXd L = MatrixXd::Identity(A.rows(), A.cols());
     // L.triangularView<Eigen::StrictlyLower>() = LU.matrixLU();
     // MatrixXd U = LU.matrixLU().triangularView<Upper>();
 
-    MatrixXd L, U;
-    lu(A, L, U);
+    // MatrixXd L, U;
+    // lu(A, L, U);
 
-    cout << "L:" << endl;
-    cout << L << endl;
-    cout << "U:" << endl;
-    cout << U << endl;
-    cout << "L*U" << endl;
-    cout << L * U << endl;
+    // cout << "L:" << endl;
+    // cout << L << endl;
+    // cout << "U:" << endl;
+    // cout << U << endl;
+    // cout << "L*U" << endl;
+    // cout << L * U << endl;
+
+    PartialPivotLU pplu(A);
+    pplu.compute();
+    x = pplu.solve(b);
+
+    // https://learnaboutstructures.com/Bernoulli-Euler-Beam-Theory
     return 0;
 }
